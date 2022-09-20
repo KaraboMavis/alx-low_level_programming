4721c016870a1975176f18404823e406f4561b21
KaraboMavis <karbsmavis13@gmail.com>
2022-09-20 10:42:38 -0700

    Strings and pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ * *reset_to_98 - resets the value of given integer to 98
+ *
+ * *@n: parameter
+ *
+ * *Return : nothing
+ *
+ * */
+
+
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
